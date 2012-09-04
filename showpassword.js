18a7b161b941e2aabfe667f661fe32a6b30b5780
Jeroen van Meerendonk <hola@jeroen.bz>
2012-09-05 01:16:39 +0200

    First commit

---
+++ showpassword.js
@@ -0,0 +1,33 @@
+/*
+*	@name							Twitter Bootstrap Show Password
+*	@descripton						
+*	@version						0.5
+*	@requires						Jquery 1.8.1
+*
+*	@author							Jeroen van Meerendonk
+*	@author-email					jeroen@cyneek.com
+*	@author-website					http://cyneek.com
+*
+*	@licens							MIT License - http://www.opensource.org/licenses/mit-license.php
+*/
+(function($){
+	$.fn.extend({
+		showPassword: function() {	
+
+			input_password	= $(this);
+
+			// Create the wrap and append the icon
+			input_password.wrap('<div class="input-append" />').after('<span class="add-on"><i class="icon-eye-open"></i></span>');
+
+			var icon_password	= input_password.next().find('.icon-eye-open');
+			icon_password.css('cursor', 'help').tooltip();
+
+			input_password.on({
+				keyup	: function() {
+					icon_password.attr('data-original-title', $(this).val());
+				}
+			});
+
+        }
+    });
+})(jQuery);
