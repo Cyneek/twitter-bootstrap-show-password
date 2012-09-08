fad4b80e9108aa3b501938d9da06f95129515909
Patroklo <patroklo@gmail.com>
2012-09-08 15:33:27 +0200

    Added call to autorefreshtooltip instead of tooltip. From now on there must be installed also the Auto Refresh Tooltip plugin

--- showpassword.js
+++ showpassword.js
@@ -1,35 +1,43 @@
 /*
 *	@name							Twitter Bootstrap Show Password
 *	@descripton						
-*	@version						0.5
+*	@version						0.2
 *	@requires						Jquery 1.8.1
 *
 *	@author							Jeroen van Meerendonk
 *	@author-email					jeroen@cyneek.com
 *	@author-website					http://cyneek.com
-*
-*	@license						MIT License - http://www.opensource.org/licenses/mit-license.php
+* 
+*	@author							Joseba Juániz
+*	@author-email					joseba@cyneek.com
+*	@author-website					http://cyneek.com
+*	@licens							MIT License - http://www.opensource.org/licenses/mit-license.php
 */
-
 (function($){
+	
+
 	$.fn.extend({
-		showPassword: function() {	
+		showPassword: function() {
+
+			
 
 			var input_password	= $(this);
 
-			// Create the wrap and append the icon
-			input_password.wrap('<div class="input-append" />').after('<span class="add-on"><i class="icon-eye-open"></i></span>');
 
-			var icon_password	= input_password.next().find('.icon-eye-open');
 
-			// Add the cursor and the tooltip
-			icon_password.css('cursor', 'help').tooltip();
-
+			//create the icon and assign 
+			var icon_password = $('<span class="add-on"><i class="icon-eye-open"></i></span>');
+			icon_password.css('cursor', 'help').autorefreshtooltip();
+			
 			input_password.on({
-				keyup	: function() {
+				input	: function() {
 					icon_password.attr('data-original-title', $(this).val());
+					icon_password.autorefreshtooltip('autoRefresh');
 				}
 			});
+			
+			// Create the wrap and append the icon
+			input_password.wrap('<div class="input-append" />').after(icon_password);
 
         }
     });
